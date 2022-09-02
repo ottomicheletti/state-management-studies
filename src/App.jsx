b6a3ae954d56e26c204359cead044e16a9a4fee0
ottomicheletti <michelettiotto@gmail.com>
2022-09-02 11:24:24 -0300

    feat: implements calculator using useState method for state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,83 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { evaluate } from 'mathjs'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [calculated, setCalculated] = useState(false);
+  const [display, setDisplay] = useState('');
+  const [operation, setOperation] = useState('');
+  const [history, setHistory] = useState([]);
+
+  const operators = ['/', '*', '-', '+'];
+  const numbersDotMinus = /\d|\.|\-/g;
+  const language = navigator.language || 'pt-BR';
+
+  const formattedValue = (result) => parseFloat(result).toLocaleString(language, {
+      useGrouping: true,
+      maximumFractionDigits: 4
+    });
+
+  const operationHandler = (value, displayValue) => {
+    if(calculated && numbersDotMinus.test(value)) {
+      setOperation(value);
+      setDisplay(displayValue);
+      setCalculated(!calculated);
+    }
+    if(operation.length === 0 && numbersDotMinus.test(value)) {
+      setOperation(operation.concat(value));
+      setDisplay(display.concat(displayValue));
+    }
+    if(!calculated && operation.length >= 1) {
+      setOperation(operation.concat(value));
+      setDisplay(display.concat(displayValue));
+    }
+  };
+
+  const equalHandler = () => {
+    if(operation === '') return;
+    if(!operators.includes(operation[operation.length - 1])) {
+      const evaluation = formattedValue(evaluate(operation));
+      setHistory([...history, { operation: display, result: evaluation }]);
+      setCalculated(true);
+    }
+  };
+
+  const clearHandler = () => {
+    setDisplay('');
+    setOperation('');
+    setCalculated(false);
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="background">
+      <div className="calculator">
+        <div className="display">
+          <p className="lastOperation">{calculated ? history[history.length - 1].operation : null}</p>
+          <div id="" className="input">{calculated ? history[history.length - 1].result : display}</div>
+        </div>
+        <div className="keys">
+          <button className="button double" onClick={() => clearHandler()}>Clear</button>
+          <button className="button op" onClick={() => operationHandler('%', '%')}>%</button>
+          <button className="button op" onClick={() => operationHandler('/', ' ÷ ')}>÷</button>
+          <button className="button" onClick={() => operationHandler('7', '7')}>7</button>
+          <button className="button" onClick={() => operationHandler('8', '8')}>8</button>
+          <button className="button" onClick={() => operationHandler('9', '9')}>9</button>
+          <button className="button op" onClick={() => operationHandler('*', ' × ')}>×</button>
+          <button className="button" onClick={() => operationHandler('4', '4')}>4</button>
+          <button className="button" onClick={() => operationHandler('5', '5')}>5</button>
+          <button className="button" onClick={() => operationHandler('6', '6')}>6</button>
+          <button className="button op" onClick={() => operationHandler('+', ' + ')}>+</button>
+          <button className="button" onClick={() => operationHandler('1', '1')}>1</button>
+          <button className="button" onClick={() => operationHandler('2', '2')}>2</button>
+          <button className="button" onClick={() => operationHandler('3', '3')}>3</button>
+          <button className="button op" onClick={() => operationHandler('-', ' - ')}>-</button>
+          <button className="button" onClick={() => operationHandler('.', '.')}>.</button>
+          <button className="button" onClick={() => operationHandler('0', '0')}>0</button>
+          <button className="button double" onClick={() => equalHandler()}>=</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
 
-export default App
+export default App;
